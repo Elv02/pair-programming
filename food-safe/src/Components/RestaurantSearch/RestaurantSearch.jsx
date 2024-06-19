14d2074dc9740b114fec94de3d69f71d9f9999c9
Lindsay Renna <lrenna.oct@gmail.com>
2024-06-19 15:48:28 -0400

    API call complete

--- food-safe/src/Components/RestaurantSearch/RestaurantSearch.jsx
+++ food-safe/src/Components/RestaurantSearch/RestaurantSearch.jsx
@@ -1,12 +1,35 @@
 import SearchResults from "../SearchResults/SearchResults";
+import axios from "axios";
+const API_URL = "http://localhost:8080/search";
 import "./RestaurantSearch.scss";
+import { useState } from "react";
 
 function RestaurantSearch() {
+	const [results, setResults] = useState(null);
+
+	async function handleSubmit(event) {
+		event.preventDefault();
+		const query = event.target.search.value;
+		event.target.reset();
+
+		await getResults(query);
+	}
+
+	async function getResults(quer) {
+		try {
+			const result = await axios.get(API_URL, { params: { q: quer } });
+			const data = result.data;
+			setResults(data);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 	return (
 		<section>
 			<div className="restaurant-search">
 				<h3>Search a Restaurant</h3>
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div className="search-field">
 						<img src="/src/assets/search.svg" alt="search icon" />
 						<input
@@ -21,7 +44,7 @@ function RestaurantSearch() {
 					</button>
 				</form>
 			</div>
-			<SearchResults />
+			<SearchResults results={results} />
 			<div className="site-promo">
 				<h4 className="promo-title">Why choose DineSafe?</h4>
 				<div className="promotion-blocks">
